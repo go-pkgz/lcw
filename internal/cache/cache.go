go-pkgz/lcw

commit 41ffddb3436f1d3a80dcc0d648546efad43f1dd5

improve v1 expirable cache memory management by recreating map on purge
Modified the `Purge` and `purge` methods to recreate the `data` map,
allowing the old map to be garbage collected, and added a new test to
check for it.

internal/cache/cache.go
@@ -158,7 +158,12 @@ func (c *LoadingCache) getValue(key string) (interface{}, bool) {
 func (c *LoadingCache) Purge() {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	for k, v := range c.data {
+
+	// to release the memory, as otherwise old map would store same amount of entries to prevent reallocations
+	oldData := c.data
+	c.data = make(map[string]*cacheItem)
+
+	for k, v := range oldData {
 		delete(c.data, k)
 		if c.onEvicted != nil {
 			c.onEvicted(k, v.data)
@@ -206,19 +211,25 @@ type keysWithTS []struct {
 func (c *LoadingCache) purge(maxKeys int64) {
 	kts := keysWithTS{}
 
-	for key, value := range c.data {
+	// to release the memory, as otherwise old map would store same amount of entries to prevent reallocations
+	oldData := c.data
+	c.data = make(map[string]*cacheItem)
+
+	for key, value := range oldData {
 		// ttl eviction
-		if time.Now().After(c.data[key].expiresAt) {
-			delete(c.data, key)
+		if time.Now().After(value.expiresAt) {
 			if c.onEvicted != nil {
 				c.onEvicted(key, value.data)
 			}
+			continue
 		}
+
+		// move non-expired items to new map
+		c.data[key] = value
 
 		// prepare list of keysWithTS for size eviction
 		if maxKeys > 0 && int64(len(c.data)) > maxKeys {
-			ts := c.data[key].expiresAt
-
+			ts := value.expiresAt
 			kts = append(kts, struct {
 				key string
 				ts  time.Time

internal/cache/cache_test.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
 	"testing"
 	"time"
@@ -200,4 +201,36 @@ func TestDoubleClose(t *testing.T) {
 	assert.NoError(t, err)
 	lc.Close()
 	lc.Close() // don't panic in case service is already closed
+}
+
+func TestBucketsLeak(t *testing.T) {
+	const n = 1_000_000
+
+	gcAndGetAllocKb := func() int {
+		stats := runtime.MemStats{}
+		runtime.GC()
+		runtime.ReadMemStats(&stats)
+		return int(stats.Alloc / 1024)
+	}
+
+	lc, err := NewLoadingCache()
+	assert.NoError(t, err)
+	allocKB := gcAndGetAllocKb()
+	t.Logf("allocated before start: %dKB\n", allocKB)
+	assert.Less(t, allocKB, 1024, "alloc should be less than 1024KB before we start")
+
+	for i := 0; i < n; i++ {
+		lc.Set(fmt.Sprintf("key-%d", i), fmt.Sprintf("val-%d", i))
+	}
+	allocKB = gcAndGetAllocKb()
+	t.Logf("alloc after storing %d entries: %dKB\n", n, allocKB)
+	assert.Greater(t, allocKB, 1024, "alloc should be more than 1024KB when we have a lot of entries")
+
+	lc.Purge()
+	allocKB = gcAndGetAllocKb()
+	t.Logf("allocated after the Purge call: %dKB\n", allocKB)
+	assert.Less(t, allocKB, 1024, "alloc should be less than 1024KB before after the Purge call")
+
+	// Prevents optimization
+	runtime.KeepAlive(lc)
 }

commit a3dbf5e9f79dadcee8d5e0926fd0164fafff03e2

remove unused delete() call, revert LoadingCache.purge changes
`purge` call became broken in 41ffddb34, this change reverts it
to the old behaviour so that memory won't be freed on its call.
It's called relatively frequently and so copying the `data` map
each time it happens would be too costly and unjustified.

internal/cache/cache.go
@@ -164,7 +164,6 @@ func (c *LoadingCache) Purge() {
 	c.data = make(map[string]*cacheItem)
 
 	for k, v := range oldData {
-		delete(c.data, k)
 		if c.onEvicted != nil {
 			c.onEvicted(k, v.data)
 		}
@@ -211,29 +210,21 @@ type keysWithTS []struct {
 func (c *LoadingCache) purge(maxKeys int64) {
 	kts := keysWithTS{}
 
-	// to release the memory, as otherwise old map would store same amount of entries to prevent reallocations
-	oldData := c.data
-	c.data = make(map[string]*cacheItem)
-
-	for key, value := range oldData {
+	for key, value := range c.data {
 		// ttl eviction
 		if time.Now().After(value.expiresAt) {
+			delete(c.data, key)
 			if c.onEvicted != nil {
 				c.onEvicted(key, value.data)
 			}
-			continue
 		}
-
-		// move non-expired items to new map
-		c.data[key] = value
 
 		// prepare list of keysWithTS for size eviction
 		if maxKeys > 0 && int64(len(c.data)) > maxKeys {
-			ts := value.expiresAt
 			kts = append(kts, struct {
 				key string
 				ts  time.Time
-			}{key, ts})
+			}{key, value.expiresAt})
 		}
 	}
 